6dbd90d0887433585b61368f9eef6f8cdb3f5a86
Marios Charidis <marioscharidis@gmail.com>
2024-09-02 20:37:54 +0300

    Added a third button

--- Digital_Museum/Form1.cs
+++ Digital_Museum/Form1.cs
@@ -21,5 +21,10 @@ namespace Digital_Museum
         {
             this.Close();
         }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
     }
 }
