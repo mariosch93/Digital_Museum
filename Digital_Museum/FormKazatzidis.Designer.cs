0a2c2d7c6d08476c468aa9079c086f52e0f9e693
mppl2308 <mppl2308@unipi.gr>
2024-09-06 13:06:26 +0300

    Kazantzidis komple

--- Digital_Museum/FormKazatzidis.Designer.cs
+++ Digital_Museum/FormKazatzidis.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormKazatzidis));
             this.pictureBoxUnipi = new System.Windows.Forms.PictureBox();
             this.pictureBoxExit = new System.Windows.Forms.PictureBox();
@@ -36,25 +37,31 @@
             this.labelbio = new System.Windows.Forms.Label();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.labelSong = new System.Windows.Forms.Label();
-            this.pictureBoxStelios = new System.Windows.Forms.PictureBox();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBoxPortraito = new System.Windows.Forms.PictureBox();
             this.richTextBox2 = new System.Windows.Forms.RichTextBox();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBoxSlides = new System.Windows.Forms.PictureBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxUnipi)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxExit)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxStelios)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxPortraito)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxSlides)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBoxUnipi
             // 
             this.pictureBoxUnipi.BackColor = System.Drawing.Color.Transparent;
             this.pictureBoxUnipi.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxUnipi.Image")));
-            this.pictureBoxUnipi.Location = new System.Drawing.Point(1345, 12);
+            this.pictureBoxUnipi.Location = new System.Drawing.Point(1793, 15);
+            this.pictureBoxUnipi.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBoxUnipi.Name = "pictureBoxUnipi";
-            this.pictureBoxUnipi.Size = new System.Drawing.Size(67, 59);
+            this.pictureBoxUnipi.Size = new System.Drawing.Size(89, 73);
             this.pictureBoxUnipi.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBoxUnipi.TabIndex = 16;
             this.pictureBoxUnipi.TabStop = false;
@@ -63,9 +70,10 @@
             // 
             this.pictureBoxExit.Cursor = System.Windows.Forms.Cursors.Hand;
             this.pictureBoxExit.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxExit.Image")));
-            this.pictureBoxExit.Location = new System.Drawing.Point(1345, 616);
+            this.pictureBoxExit.Location = new System.Drawing.Point(1793, 758);
+            this.pictureBoxExit.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBoxExit.Name = "pictureBoxExit";
-            this.pictureBoxExit.Size = new System.Drawing.Size(67, 53);
+            this.pictureBoxExit.Size = new System.Drawing.Size(89, 65);
             this.pictureBoxExit.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBoxExit.TabIndex = 17;
             this.pictureBoxExit.TabStop = false;
@@ -76,9 +84,10 @@
             this.labelLegend.AutoSize = true;
             this.labelLegend.BackColor = System.Drawing.Color.Transparent;
             this.labelLegend.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelLegend.Location = new System.Drawing.Point(393, 46);
+            this.labelLegend.Location = new System.Drawing.Point(638, 19);
+            this.labelLegend.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.labelLegend.Name = "labelLegend";
-            this.labelLegend.Size = new System.Drawing.Size(488, 25);
+            this.labelLegend.Size = new System.Drawing.Size(609, 31);
             this.labelLegend.TabIndex = 18;
             this.labelLegend.Text = "Αίθουσα αφιερωμένη στον Στέλιο Καζαντζίδη";
             // 
@@ -89,27 +98,26 @@
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Margin = new System.Windows.Forms.Padding(0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(142, 681);
+            this.panel1.Size = new System.Drawing.Size(189, 838);
             this.panel1.TabIndex = 19;
             // 
             // labelbio
             // 
             this.labelbio.AutoSize = true;
             this.labelbio.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(161)));
-            this.labelbio.Location = new System.Drawing.Point(239, 106);
-            this.labelbio.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelbio.Location = new System.Drawing.Point(320, 511);
             this.labelbio.Name = "labelbio";
-            this.labelbio.Size = new System.Drawing.Size(211, 20);
+            this.labelbio.Size = new System.Drawing.Size(259, 25);
             this.labelbio.TabIndex = 20;
             this.labelbio.Text = "Η βιογραφία του Στέλιου";
             // 
             // richTextBox1
             // 
             this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.richTextBox1.Location = new System.Drawing.Point(151, 138);
-            this.richTextBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.richTextBox1.Location = new System.Drawing.Point(236, 556);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(428, 186);
+            this.richTextBox1.Size = new System.Drawing.Size(569, 267);
             this.richTextBox1.TabIndex = 21;
             this.richTextBox1.Text = resources.GetString("richTextBox1.Text");
             // 
@@ -117,79 +125,111 @@
             // 
             this.labelSong.AutoSize = true;
             this.labelSong.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(161)));
-            this.labelSong.Location = new System.Drawing.Point(189, 354);
-            this.labelSong.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelSong.Location = new System.Drawing.Point(1070, 66);
             this.labelSong.Name = "labelSong";
-            this.labelSong.Size = new System.Drawing.Size(355, 40);
+            this.labelSong.Size = new System.Drawing.Size(439, 50);
             this.labelSong.TabIndex = 22;
             this.labelSong.Text = "Τα τραγούδια του που χάραξαν την πορεία \r\nτου Ελληνικού λαϊκού τραγουδιού!";
             // 
-            // pictureBoxStelios
+            // pictureBoxPortraito
             // 
-            this.pictureBoxStelios.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxStelios.Image")));
-            this.pictureBoxStelios.Location = new System.Drawing.Point(926, 409);
-            this.pictureBoxStelios.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.pictureBoxStelios.Name = "pictureBoxStelios";
-            this.pictureBoxStelios.Size = new System.Drawing.Size(355, 257);
-            this.pictureBoxStelios.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBoxStelios.TabIndex = 23;
-            this.pictureBoxStelios.TabStop = false;
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(752, 138);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(350, 257);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox1.TabIndex = 24;
-            this.pictureBox1.TabStop = false;
+            this.pictureBoxPortraito.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxPortraito.Image")));
+            this.pictureBoxPortraito.Location = new System.Drawing.Point(227, 130);
+            this.pictureBoxPortraito.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBoxPortraito.Name = "pictureBoxPortraito";
+            this.pictureBoxPortraito.Size = new System.Drawing.Size(578, 348);
+            this.pictureBoxPortraito.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxPortraito.TabIndex = 24;
+            this.pictureBoxPortraito.TabStop = false;
             // 
             // richTextBox2
             // 
             this.richTextBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.richTextBox2.Location = new System.Drawing.Point(163, 409);
-            this.richTextBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.richTextBox2.Location = new System.Drawing.Point(1075, 130);
+            this.richTextBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.richTextBox2.Name = "richTextBox2";
-            this.richTextBox2.Size = new System.Drawing.Size(356, 261);
+            this.richTextBox2.Size = new System.Drawing.Size(578, 348);
             this.richTextBox2.TabIndex = 25;
             this.richTextBox2.Text = "1.  Υπάρχω\n2.  Ζιγκουάλα\n3.  Κάτω απο το πουκάμισό μου\n4.  Η ζωή μου όλη\n5.  Το Σ" +
     "αβατόβραδο";
             // 
-            // pictureBox2
+            // pictureBoxSlides
             // 
-            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
-            this.pictureBox2.Location = new System.Drawing.Point(544, 409);
-            this.pictureBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(350, 257);
-            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox2.TabIndex = 26;
-            this.pictureBox2.TabStop = false;
+            this.pictureBoxSlides.Location = new System.Drawing.Point(1075, 511);
+            this.pictureBoxSlides.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBoxSlides.Name = "pictureBoxSlides";
+            this.pictureBoxSlides.Size = new System.Drawing.Size(578, 316);
+            this.pictureBoxSlides.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxSlides.TabIndex = 26;
+            this.pictureBoxSlides.TabStop = false;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(161)));
-            this.label1.Location = new System.Drawing.Point(804, 106);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(320, 91);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(267, 20);
+            this.label1.Size = new System.Drawing.Size(324, 25);
             this.label1.TabIndex = 27;
             this.label1.Text = "Πορτραίτο Στέλιου Καζαντζίδη";
             // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(1764, 503);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(118, 43);
+            this.buttonStart.TabIndex = 28;
+            this.buttonStart.Text = "Start Slides";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Location = new System.Drawing.Point(1764, 599);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(118, 37);
+            this.buttonStop.TabIndex = 29;
+            this.buttonStop.Text = "Stop Slides";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox2.TabIndex = 30;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(1704, 118);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(183, 183);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 31;
+            this.pictureBox1.TabStop = false;
+            // 
             // FormKazatzidis
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.ClientSize = new System.Drawing.Size(1424, 681);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.pictureBox2);
-            this.Controls.Add(this.richTextBox2);
+            this.ClientSize = new System.Drawing.Size(1899, 838);
             this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.pictureBoxStelios);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBoxSlides);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.pictureBoxPortraito);
             this.Controls.Add(this.labelSong);
             this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.labelbio);
@@ -197,14 +237,17 @@
             this.Controls.Add(this.labelLegend);
             this.Controls.Add(this.pictureBoxExit);
             this.Controls.Add(this.pictureBoxUnipi);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "FormKazatzidis";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "FormKazatzidis";
+            this.Load += new System.EventHandler(this.FormKazatzidis_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxUnipi)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxExit)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxStelios)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxPortraito)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxSlides)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -219,10 +262,14 @@
         private System.Windows.Forms.Label labelbio;
         private System.Windows.Forms.RichTextBox richTextBox1;
         private System.Windows.Forms.Label labelSong;
-        private System.Windows.Forms.PictureBox pictureBoxStelios;
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBoxPortraito;
         private System.Windows.Forms.RichTextBox richTextBox2;
-        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBoxSlides;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Button buttonStop;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
--- Digital_Museum/FormKazatzidis.cs
+++ Digital_Museum/FormKazatzidis.cs
@@ -12,14 +12,58 @@ namespace Digital_Museum
 {
     public partial class FormKazatzidis : Form
     {
+        private List<string> imagePaths;
+        private int currentImageIndex = 0;
         public FormKazatzidis()
         {
             InitializeComponent();
+            imagePaths = new List<string>
+            {
+                "stelios.jpg",
+                "stelios2.jpg",                
+            };
+
+            // Κατά τη φόρτωση της φόρμας, δεν ρυθμίζουμε εικόνα στο PictureBox
+            pictureBoxSlides.ImageLocation = null;
+            // Διασφάλιση ότι ο Timer δεν είναι ενεργός
+            timer1.Stop();
         }
 
         private void pictureBoxExit_Click(object sender, EventArgs e)
         {
             System.Windows.Forms.Application.Exit();
+        }
+
+        private void FormKazatzidis_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            // Ορισμός της πρώτης εικόνας πριν την εκκίνηση του Timer
+            pictureBoxSlides.ImageLocation = imagePaths[currentImageIndex];
+            // Εκκίνηση του Timer
+            timer1.Start();
+        }
+
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            // Διακοπή του Timer
+            timer1.Stop();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            // Μετάβαση στην επόμενη εικόνα
+            currentImageIndex++;
+            if (currentImageIndex >= imagePaths.Count)
+            {
+                currentImageIndex = 0; // Επαναφορά στο πρώτο
+            }
+
+            // Αλλαγή της εικόνας στο PictureBox
+            pictureBoxSlides.ImageLocation = imagePaths[currentImageIndex];
         }
     }
 }
